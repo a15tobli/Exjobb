c3cd2a61d40910933a6cb71ee58333cf0fd7076c
= <=>
2018-03-12 15:57:30 +0100

    Added form for adding images. Added image preview for the form

--- Application/JS/Navigation.js
+++ Application/JS/Navigation.js
@@ -2,21 +2,22 @@
 function setContent(clickedLink) {
     $("a").removeClass("active");
 
-    $(".splitTest").hide();
+    $(".splittestContent").hide();
+    $(".formContent").hide();
 
     switch(clickedLink){
         case "Home":
             $("#homeLink").addClass("active");
             document.getElementById("title").innerHTML="Home";
-            $(".splitTest").hide();
             break;
         case "AnswerTest":
             $("#answerLink").addClass("active");
+            $(".splittestContent").show();
             document.getElementById("title").innerHTML="Design comparison";
-            $(".splitTest").show();
             break;
         case "AddTest":
             $("#addLink").addClass("active");
+            $(".formContent").show();
             document.getElementById("title").innerHTML="Add images for new tests";
             break;
         case "ViewResult":
@@ -26,4 +27,20 @@ function setContent(clickedLink) {
         default: 
             break;
     }
+}
+
+function preview_image(event, id) 
+{
+    var reader = new FileReader();
+    reader.onload = function()
+    {
+        if(id==1){
+            var output = document.getElementById('output_image1');
+        }
+        else{
+            var output = document.getElementById('output_image2');
+        }
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
 }
