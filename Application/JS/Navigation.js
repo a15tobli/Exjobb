a481d4257ec985ed0100b86107807989d46060d2
= <=>
2018-03-12 14:26:57 +0100

    Added more functionality for dynamic content

--- Application/JS/Navigation.js
+++ Application/JS/Navigation.js
@@ -1,19 +1,27 @@
 //Displays content based on link clicked
 function setContent(clickedLink) {
     $("a").removeClass("active");
-    
+
+    $(".splitTest").hide();
+
     switch(clickedLink){
         case "Home":
             $("#homeLink").addClass("active");
+            document.getElementById("title").innerHTML="Home";
+            $(".splitTest").hide();
             break;
         case "AnswerTest":
             $("#answerLink").addClass("active");
+            document.getElementById("title").innerHTML="Design comparison";
+            $(".splitTest").show();
             break;
         case "AddTest":
             $("#addLink").addClass("active");
+            document.getElementById("title").innerHTML="Add images for new tests";
             break;
         case "ViewResult":
             $("#resultLink").addClass("active");
+            document.getElementById("title").innerHTML="View test results";
             break;
         default: 
             break;
