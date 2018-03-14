e840c1528bef015aff06f527a10ed4bef32756aa
= <=>
2018-03-14 16:12:34 +0100

    Working functionality to fetch test data from DB to php

--- application/MySQL/dbScript.sql
+++ application/MySQL/dbScript.sql
@@ -7,3 +7,10 @@ ID int AUTO_INCREMENT PRIMARY KEY,
 image longblob,
 caption varchar(100)
 )engine=innodb;
+
+CREATE TABLE Test(
+ID int AUTO_INCREMENT PRIMARY KEY,
+caption varchar(50)
+)engine=innodb;
+
+Insert INTO Test (caption) Values ("This is a test row.");
