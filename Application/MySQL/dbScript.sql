5c8102302e5e4a4c4d7f68bb6051a4c2451d553a
= <=>
2018-03-16 15:24:54 +0100

    Changes to database structure

--- Application/MySQL/dbScript.sql
+++ Application/MySQL/dbScript.sql
@@ -2,21 +2,19 @@ DROP DATABASE MySQL_DB;
 CREATE DATABASE MySQL_DB;
 USE MySQL_DB;
 
+CREATE TABLE SplitTest(
+testID int AUTO_INCREMENT PRIMARY KEY,
+testName varchar(15) UNIQUE NOT NULL 
+)engine=innodb;
+
 CREATE TABLE ImageEntry(
 ID int AUTO_INCREMENT PRIMARY KEY,
 image longblob,
-caption varchar(100)
+caption varchar(100),
+testID int,
+FOREIGN KEY (testID) REFERENCES SplitTest(testID)
 )engine=innodb;
 
-CREATE TABLE SplitTest(
-testID int AUTO_INCREMENT PRIMARY KEY,
-testName varchar(15) NOT NULL,
-imgID1 int NOT NULL,
-imgID2 int NOT NULL,
-FOREIGN KEY (imgID1) REFERENCES ImageEntry(ID),
-FOREIGN KEY (imgID2) REFERENCES ImageEntry(ID)
-)engine=innodb;
 
-INSERT INTO ImageEntry(caption) VALUES ("Testing an entry.");
-DELETE FROM ImageEntry WHERE ID=1;
-SELECT * From ImageEntry;
+SELECT * FROM SplitTest;
+SELECT * FROM ImageEntry;
