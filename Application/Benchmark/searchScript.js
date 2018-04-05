1dcc96d1a0db2b47533014bb5d3d1787d6a3b216
a15tobli <a15tobli@student.his.se>
2018-04-05 10:36:01 +0200

    Changes to search script. Collected data from pilot test

--- Application/Benchmark/searchScript.js
+++ Application/Benchmark/searchScript.js
@@ -8,13 +8,29 @@
 // @require http://code.jquery.com/jquery-2.1.0.min.js
 // ==/UserScript==
 $(document).ready(function(){
-  
-  	console.log("Script successfully loaded");
-  
-		$("#answerLink").click(function(){
-    	for (i = 0; i < 10; i++){
-        $("#searchData").val("Test1");
-      	$("#searchBtn").click();
-      }
+
+    console.log("Script successfully loaded");
+
+    //Initialize script by clicking on the link
+    $("#answerLink").click(function(){
+        var counter = 0;  
+        fillForm(counter);
     });
 });
+
+function fillForm(counter){
+    //Run 10 searches with 3 second intervals
+    if(counter < 10){
+        setTimeout(function(){
+            counter++;
+            console.log(counter);
+            $("#searchData").val("Test1");
+            $("#searchBtn").click();
+            
+            fillForm(counter);
+        }, 3000);
+    }
+    else{
+        console.log("Measurements are finished");
+    }
+}
